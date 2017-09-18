8ec218742efcfc4e74eff237cfb7bcc3e26d6b45
differance1968 <differance1968@yahoo.co.jp>
2017-09-18 11:55:33 +0000

    15th

--- db/migrate/20170827094437_add_details_to_reports.rb
+++ db/migrate/20170827094437_add_details_to_reports.rb
@@ -1,6 +1,6 @@
 class AddDetailsToReports < ActiveRecord::Migration[5.0]
   def change
-    add_column :reports, :grade_id, :integer
-    add_column :reports, :subject_id, :integer
+    add_column :reports, :grade_id, :integer, 'integer USING CAST(grade_id AS integer)'
+    add_column :reports, :subject_id, :integer, 'integer USING CAST(subject_id AS integer)'
   end
 end
--- db/migrate/20170830112551_change_datatype_grade_of_papers.rb
+++ db/migrate/20170830112551_change_datatype_grade_of_papers.rb
@@ -1,7 +1,7 @@
 class ChangeDatatypeGradeOfPapers < ActiveRecord::Migration[5.0]
   def change
      # [形式] change_column(テーブル名, カラム名, データタイプ, オプション)
-    change_column :papers, :grade_id, :integer
-    change_column :papers, :subject_id, :integer
+    change_column :papers, :grade_id, :integer, 'integer USING CAST(grade_id AS integer)'
+    change_column :papers, :subject_id, :integer, 'integer USING CAST(subject_id AS integer)'
   end
 end
