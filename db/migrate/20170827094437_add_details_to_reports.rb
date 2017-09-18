814e542ba2d391bb69967201457618c991d8bacb
differance1968 <differance1968@yahoo.co.jp>
2017-09-18 12:20:54 +0000

    16th

--- db/migrate/20170827094437_add_details_to_reports.rb
+++ db/migrate/20170827094437_add_details_to_reports.rb
@@ -1,6 +1,6 @@
 class AddDetailsToReports < ActiveRecord::Migration[5.0]
   def change
-    add_column :reports, :grade_id, :integer, 'integer USING CAST(grade_id AS integer)'
-    add_column :reports, :subject_id, :integer, 'integer USING CAST(subject_id AS integer)'
+    add_column :reports, :grade_id, :string
+    add_column :reports, :subject_id, :string
   end
 end
--- db/migrate/20170902012234_add_types_to_reports.rb
+++ db/migrate/20170902012234_add_types_to_reports.rb
@@ -1,5 +1,5 @@
 class AddTypesToReports < ActiveRecord::Migration[5.0]
   def change
-    add_column :reports, :type_id, :integer, 'integer USING CAST(type_id AS integer)'
+    add_column :reports, :type_id, :string
   end
 end
