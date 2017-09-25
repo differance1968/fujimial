a0158288309e7ac9f3e609b81a8735efb5966d68
differance1968 <differance1968@yahoo.co.jp>
2017-09-25 06:49:31 +0000

    25th

--- app/uploaders/file_uploader.rb
+++ app/uploaders/file_uploader.rb
@@ -3,9 +3,11 @@ class FileUploader < CarrierWave::Uploader::Base
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
 
-  # Choose what kind of storage to use for this uploader:
+  if Rails.env.production?
+  include Cloudinary::CarrierWave
+  else
   storage :file
-  # storage :fog
+  end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
@@ -13,6 +15,11 @@ class FileUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
+
+  
+  def public_id
+     model.id
+  end
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url(*args)
   #   # For Rails 3.1+ asset pipeline compatibility:
